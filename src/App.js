2a4e6c05129ab6f78324c7e2cde1f08a3330b0e8
belgotech <belovedobiora2002@gmail.com>
2023-07-07 15:57:18 +0100

    New Commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
 import './App.css'
 
-const App = ()=>{
-  return(
+const App = () => {
+  return (
     <div className="App">
-      <div className="Navbar">
-        <div className="Logo">
-          {/* <img src="" alt="" /> */}
-          <p>BELGO-Store</p>
-        </div>
-        <nav>
-          <ul>
-            <li><a href="#Home">Home</a></li>
-            <li><a href="#Product">Product</a></li>
-            <li><a href="#About">About</a></li>
-            <li><a href="#Contact">Contact</a></li>
-            <li><a href="#Account">Account</a></li>
-          </ul>
-        </nav>
-      </div>
+      <div className="Header">
+        <div className="Container">
+          <div className="Navbar">
+            <div className="Logo">
+              {/* <img src="" alt="" /> */}
+              <p>BELGO-Store</p>
+            </div>
+            <nav>
+              <ul>
+                <li><a href="#Home">Home</a></li>
+                <li><a href="#Product">Product</a></li>
+                <li><a href="#About">About</a></li>
+                <li><a href="#Contact">Contact</a></li>
+                <li><a href="#Account">Account</a></li>
+              </ul>
+            </nav>
+          </div>
 
-      <div className="Row">
-        <div className="Col-2">
-            <h1>Give Your Workout <br /> A New Style!</h1>
-            <p>Success is not all about greatness. It's about consistency. 
-              consistent <br /> hard work brings success. Greatness will come
-            </p>
-            <a href="#btn" className="Btn">Explore Now</a>
-        </div>
-        <div className="Col-2">
-            <img src="" alt="" />
+          <div className="Row">
+            <div className="Col-2">
+              <h1>Give Your Workout <br /> A New Style!</h1>
+              <p>Success is not all about greatness. It's about consistency.
+                consistent <br /> hard work brings success. Greatness will come
+              </p>
+              <a href="#btn" className="Btn">Explore Now &#8594;</a>
+            </div>
+            <div className="Col-2">
+              <img src="" alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
