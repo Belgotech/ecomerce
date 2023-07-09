8f9913e07df8aa9d134eefd33d5702c0f82ea66a
belgotech <belovedobiora2002@gmail.com>
2023-07-09 17:20:45 +0100

    New Commit

--- src/Component/Products.js
+++ src/Component/Products.js
@@ -1,10 +1,54 @@
 // import Img1 from '../237677.jpg'
 import Img1 from '../wallpaperflare.com_wallpaper.jpg'
 
-const Products = ()=>{
-    return(
+const Products = () => {
+    return (
         <div className="Small-container">
             <h2>Featured Products</h2>
+            <div className="Row">
+                <div className="Col-4">
+                    <img src={Img1} alt="" />
+                    <h4>Lorem ipsum dolor sit amet.</h4>
+                    <p>$50.00</p>
+                </div>
+                <div className="Col-4">
+                    <img src={Img1} alt="" />
+                    <h4>Lorem ipsum dolor sit amet.</h4>
+                    <p>$50.00</p>
+                </div>
+                <div className="Col-4">
+                    <img src={Img1} alt="" />
+                    <h4>Lorem ipsum dolor sit amet.</h4>
+                    <p>$50.00</p>
+                </div>
+                <div className="Col-4">
+                    <img src={Img1} alt="" />
+                    <h4>Lorem ipsum dolor sit amet.</h4>
+                    <p>$50.00</p>
+                </div>
+                <div className="Col-4">
+                    <img src={Img1} alt="" />
+                    <h4>Lorem ipsum dolor sit amet.</h4>
+                    <p>$50.00</p>
+                </div>
+                <div className="Col-4">
+                    <img src={Img1} alt="" />
+                    <h4>Lorem ipsum dolor sit amet.</h4>
+                    <p>$50.00</p>
+                </div>
+                <div className="Col-4">
+                    <img src={Img1} alt="" />
+                    <h4>Lorem ipsum dolor sit amet.</h4>
+                    <p>$50.00</p>
+                </div>
+                <div className="Col-4">
+                    <img src={Img1} alt="" />
+                    <h4>Lorem ipsum dolor sit amet.</h4>
+                    <p>$50.00</p>
+                </div>
+            </div>
+
+            <h2>Latest Products </h2>
             <div className="Row">
                 <div className="Col-4">
                     <img src={Img1} alt="" />
