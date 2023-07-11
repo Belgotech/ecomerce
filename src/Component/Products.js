4d52bd52f183c051e66836e83633aafe2d7ce9b0
belgotech <belovedobiora2002@gmail.com>
2023-07-11 12:06:50 +0100

    New Commit

--- src/Component/Products.js
+++ src/Component/Products.js
@@ -4,18 +4,29 @@ import Img1 from '../wallpaperflare.com_wallpaper.jpg'
 const Products = () => {
 
     const [Products, setProducts] = useState([])
+    const [error, setError] = useState([])
 
     const getProducts = async ()=>{
+        try{
         const response = await fetch('https://dummyjson.com/product')
         const json = await response.json()
+        setProducts(json.data.Products)
+    }catch(error){
+        setError(error.message)
+    }
     }
 
     useEffect(()=>{
-
-    })
+        getProducts()
+    },[])
 
     return (
         <div className="Small-container">
+            {JSON.stringify(Products.map((product, index)=>{
+                return(
+                    <div></div>
+                )
+            }))}
             <h2>Featured Products</h2>
             <div className="Row">
                 <div className="Col-4">
