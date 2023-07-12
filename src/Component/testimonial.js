73f84a5bf3cd94e9c76a7a24590865df819783f5
belgotech <belovedobiora2002@gmail.com>
2023-07-12 21:31:47 +0100

    New Commit

--- src/Component/testimonial.js
+++ src/Component/testimonial.js
@@ -4,45 +4,51 @@ import Img1 from '../img/person1.jpeg'
 import Img2 from '../img/person2.jpeg'
 import Img3 from '../img/person3.jpeg'
 
-const Testimonial =()=>{
-    return(
+const Testimonial = () => {
+    return (
         <div className="Testimonial">
             <div className="Small-container">
                 <div className="Row">
                     <div className="Col-3">
                         <FaQuoteLeft />
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae reiciendis ad debitis. Accusantium praesentium natus eius possimus in consectetur voluptate!</p>
-                        <img src={Img1} alt="" /><br />
-                        <BsStarFill />
-                        <BsStarFill />
-                        <BsStarFill />
-                        <BsStarFill />
-                        <BsStarHalf />
-                        <BsStar />
+                        <img src={Img1} alt="" />
+                        <div className="Rating">
+                            <BsStarFill />
+                            <BsStarFill />
+                            <BsStarFill />
+                            <BsStarFill />
+                            <BsStarHalf />
+                            <BsStar />
+                        </div>
                         <h3>Jony Dean</h3>
                     </div>
                     <div className="Col-3">
                         <FaQuoteLeft />
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae reiciendis ad debitis. Accusantium praesentium natus eius possimus in consectetur voluptate!</p>
-                        <img src={Img2} alt="" /> <br />
-                        <BsStarFill />
-                        <BsStarFill />
-                        <BsStarFill />
-                        <BsStarFill />
-                        <BsStarFill />
-                        <BsStarHalf />
+                        <img src={Img2} alt="" />
+                        <div className="Rating">
+                            <BsStarFill />
+                            <BsStarFill />
+                            <BsStarFill />
+                            <BsStarFill />
+                            <BsStarFill />
+                            <BsStarHalf />
+                        </div>
                         <h3>Ambrose Jane</h3>
                     </div>
                     <div className="Col-3">
                         <FaQuoteLeft />
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae reiciendis ad debitis. Accusantium praesentium natus eius possimus in consectetur voluptate!</p>
-                        <img src={Img3} alt="" /><br />
-                        <BsStarFill />
-                        <BsStarFill />
-                        <BsStarFill />
-                        <BsStarFill />
-                        <BsStarHalf />
-                        <BsStar />
+                        <img src={Img3} alt="" />
+                        <div className="Rating">
+                            <BsStarFill />
+                            <BsStarFill />
+                            <BsStarFill />
+                            <BsStarFill />
+                            <BsStarHalf />
+                            <BsStar />
+                        </div>
                         <h3>Vegan Moses</h3>
                     </div>
                 </div>
